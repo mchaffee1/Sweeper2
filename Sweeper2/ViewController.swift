fa670f7aa7a6172fb6076306310c430d4489ef2d
Michael Chaffee <mchaffee@gmail.com>
2016-10-15 11:58:59 -0700

    Initial Commit

---
+++ Sweeper2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sweeper2
+//
+//  Created by Michael Chaffee on 10/15/16.
+//  Copyright © 2016 Michael Chaffee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
